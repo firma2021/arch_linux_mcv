8c20c097eda0061346a1bb07ec2f0dd9ed5a7c3c
firma <119467443+firma2021@users.noreply.github.com>
2024-08-21 20:52:10 +0800

    chore changes

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ function init_git
     git config --global init.defaultBranch main
   fi
 
-  echo -e "${purple}设置git日志格式"
+  echo -e "${purple}设置git命令别名"
   git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ci %cr) %C(bold blue)<%an>%Creset'"
   git config --global alias.br "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate"
   git config --global alias.pu "push origin HEAD"
